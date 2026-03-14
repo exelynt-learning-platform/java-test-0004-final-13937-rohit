e2c7f264791f8f9967a6cdf521f280501ed503a7
rohit-shete92 <rohitshete7018@gmail.com>
2026-03-14 18:25:00 +0530

    Add pattern printing program in Main.java
    Implement a program to print a pattern based on input size.

---
+++ Main.java
@@ -0,0 +1,34 @@
+class Main {
+    public static void main(String[] args) {
+
+        int n = 4;
+        int size = 2 * n - 1;
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+
+                int top = i;
+                int left = j;
+                int bottom = size - 1 - i;
+                int right = size - 1 - j;
+
+                int min = top;
+
+                if (left < min) {
+                    min = left;
+                }
+                if (bottom < min) {
+                    min = bottom;
+                }
+                if (right < min) {
+                    min = right;
+                }
+
+                System.out.print((n - min) + " ");
+            }
+
+            System.out.println();
+        }
+
+    }
+}
